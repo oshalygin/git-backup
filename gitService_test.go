0d7d05c5e0adb2edd37922fce9b55cdb44ce4a39
Oleg Shalygin <oshalygin@gmail.com>
2017-07-25 23:29:01 -0700

    chore(gitService_test): remove comments
    - Removing commented code that was a remnant of
      a working solution

--- gitService_test.go
+++ gitService_test.go
@@ -196,11 +196,3 @@ func Test_GitService(t *testing.T) {
 
 	})
 }
-
-// func fakeExecCommand(command string, args...string) *exec.Cmd {
-//     cs := []string{"-test.run=TestHelperProcess", "--", command}
-//     cs = append(cs, args...)
-//     cmd := exec.Command(os.Args[0], cs...)
-//     cmd.Env = []string{"GO_WANT_HELPER_PROCESS=1"}
-//     return cmd
-// }
